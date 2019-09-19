c58311265afb7289d54bec9120c523ce141b1233
zxfjd3g <258147149@qq.com>
2019-09-19 11:35:33 +0800

    静态组件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Header from './components/Header/Header.vue'
 
 Vue.config.productionTip = false
+
+// 注册全局组件标签
+Vue.component('Header', Header)
 
 new Vue({
   render: h => h(App),
